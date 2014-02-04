ac3b822c3fca704e4be47e57763acb7183efaffb
joon <jonatan.van.hove@gmail.com>
2014-02-04 17:59:58 +0100

    duplicate function
    I probably messed up the pull request

--- tv/ouya/console/api/OuyaController.hx
+++ tv/ouya/console/api/OuyaController.hx
@@ -155,17 +155,6 @@ class OuyaController
 	public function getName():Dynamic
 	{
 		if (_getName_func == null)
-		_getName_func = openfl.utils.JNI.createMemberMethod("android.view.InputDevice", "getName", "()Ljava/lang/String;", true);
-		var a = new Array<Dynamic>();
-		a.push (getDevice());
-		return _getName_func(a);
-	}
-	
-	private static var _getName_func:Dynamic;
-
-	public function getName():Dynamic
-	{
-		if (_getName_func == null)
 			_getName_func = openfl.utils.JNI.createMemberMethod("android.view.InputDevice", "getName", "()Ljava/lang/String;", true);
 		var a = new Array<Dynamic>();
 		a.push (getDevice());
